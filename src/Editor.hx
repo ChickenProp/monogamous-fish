05729b49409752475b05e461625d14e1691344fe
Philip Hazelden <philip.hazelden@gmail.com>
2012-08-16 17:13:11 +0100

    Make the editor work mouse-based, not keyboard-based.
    Exception: +/- still needed to edit allowed changes.
    
    Bug: if you copy with the mouse over the level, it copies whatever type
    of tile you're currently placing instead of what's actually there.

--- src/Editor.hx
+++ src/Editor.hx
@@ -8,70 +8,72 @@ import com.haxepunk.graphics.Text;
 import com.haxepunk.graphics.Spritemap;
 
 class Editor extends MyWorld {
-	public var selX:Int;
-	public var selY:Int;
+	public var panel:Panel;
+	public var entToPlace:Entity;
+
+	public var tiles:Array<Array<Entity>>;
 
 	public function new () {
 		super();
+		tiles = [];
 		width = 18;
 		height = 13;
 		for (x in 0...width) {
+			tiles.push([]);
 			for (y in 0...height) {
-				add(new Rock(30*x, 30*y));
+				var r = new Rock(30*x, 30*y);
+				tiles[x].push(r);
+				add(r);
 			}
 		}
-		selX = 8;
-		selY = 6;
 
 		addChangeCount();
 
-		add(new Panel());
+		panel = cast add(new Panel());
 	}
 
+	var hiddenTile:Entity;
 	override public function update () : Void {
 		super.update();
-
-		var dx = (Input.pressed(Key.RIGHT) ? 1 : 0)
-			- (Input.pressed(Key.LEFT) ? 1 : 0);
-		var dy = (Input.pressed(Key.DOWN) ? 1 : 0)
-			- (Input.pressed(Key.UP) ? 1 : 0);
-		selX += dx;
-		selY += dy;
-		selX = Std.int(HXP.clamp(selX, 0, width - 1));
-		selY = Std.int(HXP.clamp(selY, 0, height - 1));
-
-		if (Input.check(Key.SPACE))
-			removeTile(selX, selY);
-		if (Input.check(Key.R)) {
-			removeTile(selX, selY);
-			add(new Rock(selX*30, selY*30));
-		}
-		if (Input.check(Key.M)) {
-			removeTile(selX, selY);
-			add(new Fish(selX*30, selY*30, false));
-		}
-		if (Input.check(Key.F)) {
-			removeTile(selX, selY);
-			add(new Fish(selX*30, selY*30, true));
-		}
 
 		var dc = (Input.pressed(187) ? 1 : 0) // plus (including equals)
 			- (Input.pressed(189) ? 1 : 0); // minus
 		allowedChanges += dc;
 		if (allowedChanges < 0)
 			allowedChanges = 0;
+
+		var tx = pointToTile(mouseX);
+		var ty = pointToTile(mouseY);
+
+		if (hiddenTile != null)
+			add(hiddenTile);
+
+		if (0 <= tx && tx < width && 0 <= ty && ty < height) {
+			hiddenTile = tiles[tx][ty];
+			if (hiddenTile != null)
+				remove(hiddenTile);
+
+			if (entToPlace != null) {
+				entToPlace.x = tx*30;
+				entToPlace.y = ty*30;
+			}
+
+			if (Input.mouseDown) {
+				swapTile(tx, ty, panel.addSelectedEntity(tx*30, ty*30));
+				hiddenTile = null;
+			}
+		}
+		else
+			hiddenTile = null;
 	}
 
-	public function removeTile(x:Int, y:Int) : Void {
-		var ents:Array<Entity> = [];
-		collidePointInto("tile", x*30, y*30, ents);
-		for (e in ents)
-			remove(e);
+	public function swapTile (x:Int, y:Int, e:Entity) : Void {
+		if (tiles[x][y] != null)
+			remove(tiles[x][y]);
+		tiles[x][y] = e;
 	}
 
-	override public function render () : Void {
-		super.render();
-
-		Draw.circlePlus(selX*30, selY*30, 14, 0xFFFFFF, 1, false, 2);
+	public function pointToTile (p:Float) : Int {
+		return Math.floor((p+15)/30);
 	}
 }
--- src/Panel.hx
+++ src/Panel.hx
@@ -2,9 +2,17 @@ import com.haxepunk.Entity;
 import com.haxepunk.graphics.Graphiclist;
 import com.haxepunk.graphics.Image;
 import com.haxepunk.graphics.Spritemap;
+import com.haxepunk.utils.Input;
+
+typedef Button = {
+	var ent:Entity;
+	var type:String;
+};
 
 class Panel extends Entity {
-	public var buttons:Array<Entity>;
+	public var buttons:Array<Button>;
+	public var editor:Editor;
+	public var selectedButton:Button;
 
 	public function new () {
 		super();
@@ -25,17 +33,20 @@ class Panel extends Entity {
 	}
 
 	override public function added () : Void {
-		addButton(0);
-		addButton(1);
-		addButton(2);
-		addButton(3);
+		editor = cast world;
+		addButton(0, "empty");
+		addButton(1, "rock");
+		addButton(2, "male");
+		addButton(3, "female");
+		selectButton(buttons[0]);
 	}
 
-	public function addButton(frame:Int) : Void {
+	public function addButton(frame:Int, type:String) : Void {
 		var i = 0;
 		var b = new Entity();
 		b.x = x;
 		b.y = y + 30*buttons.length;
+		b.width = b.height = 30;
 
 		var s = new Spritemap("gfx/tiles.png", 30, 30);
 		s.frame = frame;
@@ -43,7 +54,50 @@ class Panel extends Entity {
 		b.graphic = s;
 		b.layer--;
 
-		buttons.push(b);
+		buttons.push({ ent: b, type: type});
 		world.add(b);
+	}
+
+	public function createEntity (type:String) : Entity {
+		switch (type) {
+		case "male": return new Fish(0, 0, false);
+		case "female": return new Fish(0, 0, true);
+		case "rock": return new Rock(0, 0);
+		default: return null;
+		}
+	}
+
+	public function addSelectedEntity (x:Float, y:Float) : Entity {
+		var e = createEntity(selectedButton.type);
+		if (e != null) {
+			world.add(e);
+			e.x = x;
+			e.y = y;
+		}
+		return e;
+	}
+
+	public function selectButton (b:Button) {
+		selectedButton = b;
+
+		if (editor.entToPlace != null)
+			world.remove(editor.entToPlace);
+
+		editor.entToPlace = createEntity(b.type);
+
+		if (editor.entToPlace != null)
+			world.add(editor.entToPlace);
+	}
+
+	override public function update () : Void {
+		var mx = Input.mouseX;
+		var my = Input.mouseY;
+
+		if (Input.mousePressed) {
+			for (b in buttons) {
+				if (b.ent.collidePoint(b.ent.x, b.ent.y, mx,my))
+					selectButton(b);
+			}
+		}
 	}
 }
